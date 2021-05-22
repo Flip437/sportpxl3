b3a4d9892305481ab67ee779c732689e8c4802af
Flip437 <philippediollot@hotmail.fr>
2021-05-22 18:06:03 +0200

    working on sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 
 
 //= require template/jquery.min
+//= require template/jquery-3.2.1.min
 //= require template/jquery-migrate-3.0.1.min
 //= require template/popper.min
 //= require template/bootstrap.min
